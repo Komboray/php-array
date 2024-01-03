c68e0f7262cca410623f86321637121942d6cd01
Mbithi Raymond Kombo <121002402+Komboray@users.noreply.github.com>
2024-01-03 07:21:40 +0300

    Add files via upload

---
+++ php-array/index.php
@@ -0,0 +1,25 @@
+<?php
+//we are trying to do ph array indexing to develop our learning skills
+
+
+$food = array("pizza", "ugali", "matumbo");
+
+//this one prints out the pizza variable
+echo "$food[0] </br>"; 
+//this one prints out the ugali variable
+echo "$food[1] </br>"; 
+//this one prints out the matumbo variable
+echo "$food[2] </br>";
+
+
+//we are now going to try a different way of array echoing
+$fruits = [
+    
+    "mango" => "kili"
+];
+
+//this one prints out the kili
+echo $fruits["mango"];
+
+
+?>
